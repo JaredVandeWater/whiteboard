ccc077d78cad2c50efb9e19eaaa9d982e16cae41
Jared VandeWater <jaredvande@gmail.com>
2021-05-19 09:16:39 -0600

    palindrome

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function longestWord(str) {
     let words = str.split(' ')
     let longestWord=''
@@ -9,4 +8,22 @@ function longestWord(str) {
     }
    console.log(longestWord);
 }
-longestWord("What is the longest word in this string")
+//longestWord("What is the longest word in this string")
+
+
+
+function isPalindrome(str){
+    matches = true
+    reversed = str.split('').reverse().join('')
+    for (let i = 0; i < reversed.length; i++) {
+        const currentLetter = reversed[i];
+        if(currentLetter === str[i]){
+            matches=true
+        }else matches = false
+
+    }
+    return console.log(matches);
+}
+
+isPalindrome('racecar')
+isPalindrome('sdfsffffff')
